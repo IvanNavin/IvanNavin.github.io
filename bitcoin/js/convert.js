fb901da81480b58d4e1a9aeab3ebb5bb07d6217a
IvanNavin <e0637242529@gmail.com>
2018-10-18 07:43:25 +0300

    Переместил коментарий Прайс

--- bitcoin/js/convert.js
+++ bitcoin/js/convert.js
@@ -1,10 +1,10 @@
 // Bitcoin
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD', true);
-// Price
 xhr.onreadystatechange = function(){
 	if (xhr.readyState == 4){
 		var ticker = JSON.parse(xhr.responseText);
+		// Price
 		var price = ticker.last;
 		document.getElementById('btc').innerHTML = price;
 	}
